6726b33188f88ea516e8ba76bc31b01a0db0eca0
GretaDam <azem.damien@live.fr>
2021-11-18 15:11:55 +0100

    15th commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,10 @@
             <p>Gardez l'esprit tranquille en ajoutant toutes vos tâches à votre to-do list, où que vous soyez et quel que soit l'appareil utilisé grâce à votre compte personnel.</p>
         </div>
         <div id="index">
-            <div class="indexButton">
+            <div class="indexButton btn">
                 <a href="{{route('login')}}">Se connecter</a>
             </div>
-            <div class="indexButton">
+            <div class="indexButton btn">
                 <a href="{{route('register')}}">Créer un compte</a>
             </div>
         </div>
