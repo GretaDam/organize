df6670754f0168ac8f35d98efd3deed896003870
GretaDam <damien.azem@gretasudchampagne.com>
2021-11-22 11:50:53 +0100

    27th commit

--- resources/views/user_connect.blade.php
+++ resources/views/user_connect.blade.php
@@ -26,9 +26,6 @@
             </div>
 
             <div id="alertLog">
-                @if ($errors->any())
-                    <p class="alert">{{ $errors->first('auth') }}</p>
-                @endif
             </div>
             
             <div class="inputSubmit">
